0be4e0c3a9cfe98d78c53ff35da87ef78df61fd5
ACluelessDeveloper <acluelessdev@gmail.com>
2026-08-10 22:45:30 +0800

    Added a scroll indicator
    Exactly what it says on the commit title; I added a small but definitely not simple arrow indicator to let the user know they can scroll. It's more aesthetic than it is practical. Had to look up a tutorial for it.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 window.addEventListener("scroll", function () {
     const topBar = document.querySelector(".top-bar");
+    const scrolldownIndicator = document.querySelector(".scroll-down-indicator");
 
-    if (window.scrollY >= 10) {
+    if (window.scrollY >= 200) {
         topBar.classList.add("topbar-hide");
+        scrolldownIndicator.classList.add("fadeout-scroll-indicator");
     } else {
         topBar.classList.remove("topbar-hide");
+        scrolldownIndicator.classList.remove("fadeout-scroll-indicator");
     }
 });
