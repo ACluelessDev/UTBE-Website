bde488d0ab13d78d555441ab406450886a76684e
ACluelessDeveloper <acluelessdev@gmail.com>
2026-08-20 11:37:43 +0800

    Made a lot of adjustments and additions, focusing mainly on the WHAT portion
    First additions were the Placeholder Portrait which will be used later in the WHO section. The portrait belongs to developer 'SAI' for a character mod in the game 'Rivals of Aether' depicting the character 'Mr. Red' from the 'Shock' animation series by artist 'Terkoiz'. All credits belong to the respective owners.
    
    Adjusted the scroll down indicator arrows to be more smoother since their animation seemed a bit buggy and jagged
    
    Added the portrait to the WHAT section, although not final since it belongs in the WHO section, and implemented a pan-in-from-left animation for both the WHAT category text and the portrait itself to test out both
    
    Added a script in the JavaScript file that will play the pan-in animation ONLY when the user scrolls to that specific portion of the website -- this was achived via IntersectionObserver, please check the file to see who is credited for the tutorial
    
    Added media queries used to optimize the website for other devices, may need some adjustment since the values are slightly hard-coded and there are only two device resolutions it's accounting for -- once again, check the CSS file to see who is credited towards the tutorial
    
    And lastly, reformatted most of the CSS code to look less painful on the eyes
    
    Next natural step is to add some Lorem Ipsum placeholder text to the WHAT section to act as a description and add some more flare and aesthetic to the left side of the WHAT section to simulate what the final section would look like before applying what was done to the rest of the sections (WHO, WHY, HOW)

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ window.addEventListener("scroll", function () {
         scrolldownIndicator.classList.remove("fadeout-scroll-indicator");
     }
 });
+
+/* Had to look up a tutorial on how to make an animation play SPECIFICALLY when it's being viewed
+    This can be achieved via IntersectionObserver.
+    Credits go to YouTuber "Coding2GO" found on this video: https://youtu.be/Jo8ABAJtMM0
+    I just changed it a bit to fit with multiple parts of my "WHAT" portion
+ */
+const whatSection = document.querySelector(".utbe-what");
+const whatText = document.querySelector(".utbe-what-text");
+const whatPortrait = document.querySelector(".utbe-what-portrait");
+
+if (whatSection && whatText && whatPortrait) {
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    whatText.classList.add("is-visible");
+                    whatPortrait.classList.add("is-visible");
+
+                    observer.unobserve(entry.target);
+                }
+            });
+        },
+        {
+            threshold: 0.25
+        }
+    );
+
+    observer.observe(whatSection);
+}
